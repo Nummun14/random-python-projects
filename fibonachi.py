59c3d447daff17fe70dc90124ca7a38780cc1418
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-04-21 23:21:49 +0300

    made length changeable

--- fibonachi.py
+++ fibonachi.py
@@ -2,7 +2,8 @@ fibo1 = 1
 fibo2 = 1
 print(fibo1)
 print(fibo2)
-for i in range(10):
+length = int(input("how long?"))
+for i in range(length // 2):
     fibo1 = fibo1 + fibo2
     print(fibo1)
     fibo2 = fibo1 + fibo2
