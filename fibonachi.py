a3a4648d0a33987814cbf5756da4326db4fbb20f
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-05-30 18:27:14 +0300

    Fibonachi order (#24)
    * Create fibonachi.py
    
    * made length changeable
    
    * reordered

--- fibonachi.py
+++ fibonachi.py
@@ -1,8 +1,8 @@
+length = int(input("how long?"))
 fibo1 = 1
 fibo2 = 1
 print(fibo1)
 print(fibo2)
-length = int(input("how long?"))
 for i in range(length // 2):
     fibo1 = fibo1 + fibo2
     print(fibo1)
