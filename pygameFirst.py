51b917b63912d0df66f8a07d1eb527c818a06507
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-06-02 00:07:56 +0300

    adjusted screen size

--- pygameFirst.py
+++ pygameFirst.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode([300, 600])
+screen = pygame.display.set_mode([640, 480])
 screen.fill((0, 0, 0))
 pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
 pygame.display.flip()
