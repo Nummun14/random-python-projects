64e7c940ada6716180861816c497604c753d4c01
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-06-02 18:15:49 +0300

    finished rectangle art

--- pygamestuff/rectangleArt.py
+++ pygamestuff/rectangleArt.py
@@ -7,18 +7,18 @@ screen.fill("white")
 pygame.display.flip()
 running = True
 
+for i in range(150):
+    x = random.randint(0, 500)
+    y = random.randint(0, 400)
+    width = random.randint(0, 250)
+    height = random.randint(0, 100)
+
+    pygame.draw.rect(screen, "black", [x, y, width, height], 1)
+pygame.display.flip()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    for i in range(100):
-        x = random.randint(0, 500)
-        y = random.randint(0, 400)
-        width = random.randint(0, 250)
-        height = random.randint(0, 100)
-
-        pygame.draw.rect(screen, "white", [x, y, width, height], 2)
-    pygame.display.flip()
 
 pygame.quit()
