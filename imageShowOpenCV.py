a21e22fe3f14955b239ae712bdc8cf138ab2eafe
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-08-14 14:12:40 +0300

    added webcamExperimentation

--- imageShowOpenCV.py
+++ imageShowOpenCV.py
@@ -2,13 +2,13 @@ import cv2
 
 # Read an image from file
 img = cv2.imread("N.jpg")
-img = cv2.resize(img, (0, 0), fx=1.1, fy=1.1)
+img = cv2.resize(img, (500, 500))
 img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
+cropped_img = img[100:450, 100:450]
 
 # Display the image in a window
 cv2.imshow('image', img)
-
+cv2.imshow('croppedImage', cropped_img)
 
 # Wait for a key press and then close the window
 cv2.waitKey(0)
